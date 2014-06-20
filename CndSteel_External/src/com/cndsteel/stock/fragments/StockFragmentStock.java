6fbb254c7bf59ff635dba5bc8f806b5ea3f50c5d
john zhang <zhangxianglu1984@gmail.com>
2014-06-20 21:17:14 +0800

    同步代码

--- CndSteel_External/src/com/cndsteel/stock/fragments/StockFragmentStock.java
+++ CndSteel_External/src/com/cndsteel/stock/fragments/StockFragmentStock.java
@@ -2,7 +2,7 @@ package com.cndsteel.stock.fragments;
 
 import java.util.ArrayList;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
