86a2363fdc8618555a7f95978923ec58e02e8902
john zhang <zhangxianglu1984@gmail.com>
2014-06-25 10:37:30 +0800

    sad

---
+++ CndSteel_External/src/com/cndsteel/framework/constant/QueryParams.java
@@ -0,0 +1,23 @@
+package com.cndsteel.framework.constant;
+
+/**
+ * 请求参数名
+ * @author zhxl
+ *
+ */
+public interface QueryParams {
+	
+	/**
+	 * 公用
+	 */
+	public static final String QUERY_PARAM_SESSION_ID = "sessionId";
+	public static final String QUERY_PARAM_PAGE_INDEX = "pageindex";
+	public static final String QUERY_PARAM_PAGE_SIZE = "pagesize";
+
+	/**
+	 * 计划模块
+	 */
+	public static final String QUERY_PARAM_PLAN_STATUS = "status";
+	public static final String QUERY_PARAM_PLAN_DATE_YEAR = "year";
+	public static final String QUERY_PARAM_PLAN_DATE_MONTH = "month";
+}
